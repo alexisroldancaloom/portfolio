3d6767b4bf23a5be79ce2807c221e908632f092f
Alexis Roldán Sánchez <alexis.roldan@necsus.com.mx>
2024-09-13 08:21:10 -0600

    Updated deploy

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_WaS2pcYf.mjs';
-import { manifest } from './manifest_BC6v9lOD.mjs';
+import { manifest } from './manifest_CJYEKRWV.mjs';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "d6e90996-e5e2-4e8b-bdbf-d2b559f4bb77",
+    "middlewareSecret": "841dbef4-6afa-4e9f-83f2-4eb62e00b56a",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
