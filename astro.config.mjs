6e1c508972cc1583b5d07373e83e2c012fdb5622
Alexis Roldán Sánchez <alexis.roldan@necsus.com.mx>
2024-09-13 09:13:24 -0600

    again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
+import "../portfolioAlexis/src/global.css";
 
 export default defineConfig({
   output: "server",
