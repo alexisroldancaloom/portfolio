7ff3e922faaa8d2f41deacd03f8424d8f7738221
Alexis Roldán Sánchez <alexis.roldan@necsus.com.mx>
2024-09-12 23:51:17 -0600

    Fix issue and build

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_WaS2pcYf.mjs';
+import { manifest } from './manifest_BC6v9lOD.mjs';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "d6e90996-e5e2-4e8b-bdbf-d2b559f4bb77",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
