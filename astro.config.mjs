03f8b4575e63155776f54055bc453ce5de758943
Alexis Roldan S. <57693647+alexisroldancaloom@users.noreply.github.com>
2026-04-20 23:51:05 +0000

    Update Portfolio 20-04-2026

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: "https://alexisroldan.vercel.app",
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
